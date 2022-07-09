850da081e894bc3eaa00e7b332920bf8dddd53a4
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2022-07-09 14:06:50 +0100

    Show movie review excerpts; Refactor Details page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -21,6 +21,17 @@ export const getMovies = () => {
       .then(res => res.json())
       .then(json => json.genres);
   };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
   
   export const getMovieImages = (id) => {
     return fetch(
