6821b59cdb77fa1f0869ddc3e8bddfa0e460f482
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2022-07-09 15:24:07 +0100

    Added Upcoming movies page.

---
+++ src/pages/upcomingMoviesPage.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import PageTemplate from '../components/templateMovieListPage';
+import { getUpcomingMovies } from "../api/tmdb-api";
+
+const UpcomingMoviesPage = (props) => {
+  const [movies, setMovies] = useState([]);
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  useEffect(() => {getUpcomingMovies().then(movies => {setMovies(movies);});}, []);
+  return (
+    <PageTemplate
+      title='Upcoming Movies'
+      movies={movies} />
+  );
+};
+export default UpcomingMoviesPage; 
