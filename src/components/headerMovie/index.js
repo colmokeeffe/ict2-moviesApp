a72d8a5e98e4f1a66270d8ec9c510fd02f837d79
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2022-07-09 14:34:17 +0100

    Fix backward/foeward arrows

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -6,6 +6,9 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,7 +29,7 @@ const MovieHeader = (props) => {
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back"  onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
@@ -38,7 +41,7 @@ const MovieHeader = (props) => {
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+      <IconButton aria-label="go forward"  onClick={() => navigate(1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
