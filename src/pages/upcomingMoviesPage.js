ba596c1ad3bcc31037fda99c6bef1ed05735b58f
Colm O' Keeffe <77028267+colmokeeffe@users.noreply.github.com>
2022-07-17 00:43:02 +0100

    Change upcoming movies page to use caching.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from 'react-query'; 
 import Spinner from '../components/spinner';
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
+
 import PlaylistAddIcon from  "@material-ui/icons/PlaylistAdd"
 
 const addToFavourites = () => null;
